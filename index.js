27aba2143d559903f16a3065aa698d93852279cb
Rohandeep512 <rohandeep512@gmail.com>
2025-06-07 12:01:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var randomNumber1=Math.floor((6*Math.random())+1);
-document.querySelector(".img1").setAttribute("src","./images/dice"+randomNumber1+".png");
+document.querySelector(".img1").setAttribute("src","./dice"+randomNumber1+".png");
 
 var randomNumber2=Math.floor((6*Math.random())+1);
-document.querySelector(".img2").setAttribute("src","./images/dice"+randomNumber2+".png");
+document.querySelector(".img2").setAttribute("src","./dice"+randomNumber2+".png");
 
 if(randomNumber1>randomNumber2){
     document.querySelector("h1").innerHTML="👑 Player 1 Wins!"
