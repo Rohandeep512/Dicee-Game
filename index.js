3ed8dcc6142a7dff369488d0e1bb86ae65a48ee8
Rohandeep512 <rohandeep512@gmail.com>
2025-06-07 11:53:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var randomNumber1=Math.floor((6*Math.random())+1);
+document.querySelector(".img1").setAttribute("src","./images/dice"+randomNumber1+".png");
+
+var randomNumber2=Math.floor((6*Math.random())+1);
+document.querySelector(".img2").setAttribute("src","./images/dice"+randomNumber2+".png");
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="👑 Player 1 Wins!"
+}
+else if(randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML="Player 2 Wins! 👑"
+}
+else{
+    document.querySelector("h1").innerHTML="Draw! 🏳️"
+}
